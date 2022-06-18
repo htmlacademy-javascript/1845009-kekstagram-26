3a0be759fad2de82637fa0a459ccd1f0edd9cbac
andrei110295@yandex.ru <andrei110295@yandex.ru>
2022-06-19 00:04:07 +0300

    Переписал функцию генерации нового значения без повтора и убрал лишнюю функцию

--- js/data.js
+++ js/data.js
@@ -1,5 +1,4 @@
 import {getRandomPositiveInteger} from './util.js';
-import {getArray} from './util.js';
 import {getRandomArrayElement} from './util.js';
 import {getElementNoRepeat} from './util.js';
 
@@ -58,19 +57,14 @@ const getMessage = function() {
   return message;
 };
 
-const PHOTO_ID = getArray(1 , 25);
-const PHOTO_URL = getArray(1, 25);
-const COMMENTS_ID = [];
+const getPhotoId = getElementNoRepeat(1, 25);
+const getUrl = getElementNoRepeat(1, 25);
+const getIdComment = getElementNoRepeat(100, 200);
 
 // создание комментария
 const createComment = function () {
-  let idComment;
-  do {
-    idComment = getRandomPositiveInteger(1, 150);
-  } while (COMMENTS_ID.includes(idComment));
-  COMMENTS_ID.push(idComment);
   return {
-    id: idComment,
+    id: getIdComment(),
     avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
     message: getMessage(),
     name: `${getRandomArrayElement(NAMES)} ${getRandomArrayElement(SURNAMES)}`,
@@ -80,8 +74,8 @@ const createComment = function () {
 // создание описания к фотографии
 const createFotoDescription = function () {
   return {
-    id: getElementNoRepeat(PHOTO_ID),
-    url: `photo/${getElementNoRepeat(PHOTO_URL)}.jpg`,
+    id: getPhotoId(),
+    url: `photo/${getUrl()}.jpg`,
     description: getRandomArrayElement(DESCRIPTIONS),
     likes: getRandomPositiveInteger(15, 200),
     comments: Array.from({length: getRandomPositiveInteger(1, 3)}, createComment),
--- js/util.js
+++ js/util.js
@@ -11,27 +11,28 @@ const checkStringLength = function (userString, maxLength) {
   return !(userString.length > maxLength);
 };
 
-// создание массива значений в заданном диапазоне
-const getArray = function (startValue, lastValue) {
-  const idArray = [];
-  for (let i = startValue; i <= lastValue; i++) {
-    idArray.push(i);
-  }
-  return idArray;
-};
-
 // получение случайного значения из массива
 const getRandomArrayElement = function (elements) {
   return elements[getRandomPositiveInteger(0, elements.length - 1)];
 };
 
-// получение и удаление (с целью избежания повторения) случайного значения из массива
-const getElementNoRepeat = function (array) {
-  return array.splice(getRandomPositiveInteger(0, array.length - 1), 1)[0];
+// получение случайного значения без повтора в диапазоне
+const getElementNoRepeat = function (min, max) {
+  const numbers = [];
+  return function () {
+    let randomNumber = getRandomPositiveInteger(min, max);
+    if (numbers.length >= (max - min + 1)) {
+      return null;
+    }
+    while (numbers.includes(randomNumber)) {
+      randomNumber = getRandomPositiveInteger(min, max);
+    }
+    numbers.push(randomNumber);
+    return randomNumber;
+  };
 };
 
 export {getRandomPositiveInteger};
 export {checkStringLength};
-export {getArray};
 export {getRandomArrayElement};
 export {getElementNoRepeat};
